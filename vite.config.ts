e41401285007c146c8d1171a68e9856e4c0deb5c
yanisberkane <yanis.berkane@epitech.eu>
2025-12-18 23:37:50 +0100

    feat: Introduce Docker setup with Nginx for deployment and configure Vite's base path.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/dev/visual-editor/playground/',
   plugins: [react()],
   server: {
     allowedHosts: true,
